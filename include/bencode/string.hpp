7242805f52931a25ddb338d96f0dd3b049625fb3
Yakau Bubnou <yakau_bubnou@epam.com>
2016-03-10 20:28:14 +0300

    Bencode string a few docstrings.

--- include/bencode/string.hpp
+++ include/bencode/string.hpp
@@ -25,33 +25,56 @@ private:
 public:
     using string_type = std::basic_string<CharT, Traits, allocator_type>;
 
+    using size_type = typename string_type::size_type;
+
     using iterator = typename string_type::iterator;
 
     using const_iterator = typename string_type::const_iterator;
 
+    /**
+     *  @brief Construct an empty string.
+     */
     basic_string()
     { }
 
+    /**
+     *  @brief Construct a string using downcasted reference to the basic type.
+     *  @param __value  Reference to the basic type.
+     */
     basic_string(const basic_value_type& __value)
     {
-        auto __string = dynamic_cast<
-            const basic_string&>(__value);
-
+        auto __string = dynamic_cast<const basic_string&>(__value);
         _M_value = __string._M_value;
     }
 
+    /**
+     *  @brief Construct a string using downcasted pointer to the basic type.
+     *  @param __ptr  Shared pointer to the basic type.
+     */
     basic_string(const std::shared_ptr<basic_value_type>& __ptr)
     : basic_string(*__ptr)
     { }
 
-    basic_string(const basic_string& __string)
-    : _M_value(__string._M_value)
+    /**
+     *  @brief Construct a string as copy of @a str.
+     *  @param __str  Source string.
+     */
+    basic_string(const basic_string& __str)
+    : _M_value(__str._M_value)
     { }
 
-    basic_string(const string_type& __s)
-    : _M_value(__s)
+    /**
+     *  @brief Construct a string as a copy of @a str.
+     *  @param __str  Source string.
+     */
+    basic_string(const string_type& __str)
+    : _M_value(__str)
     { }
 
+    /**
+     *  @brief Construct a string initialized by a character %array.
+     *  @param __chars  Source character array.
+     */
     basic_string(const CharT* __chars)
     : _M_value(__chars)
     { }
@@ -59,15 +82,21 @@ public:
     ~basic_string()
     { }
 
-    // Serialize the basic_string value to the specified output stream.
+    /**
+     *  @brief Serialize the basic_string value to the specified output stream.
+     *  @param __s  Reference to the output stream.
+     */
     void
-    dump(std::basic_ostream<CharT, Traits> &__s) const
+    dump(std::basic_ostream<CharT, Traits>& __s) const
     { __s << _M_value.length() << basic_value_type::delimiter_token
         << _M_value; }
 
-    // Deserialize the basic_string value from the specified input stream.
+    /**
+     *  @brief Deserialize the basic_string value from the specified input stream.
+     *  @param __s  Reference to the input stream.
+     */
     void
-    load(std::basic_istream<CharT, Traits> &__s)
+    load(std::basic_istream<CharT, Traits>& __s)
     {
         // Define the integer symbol representation placeholder.
         std::basic_stringstream<CharT, Traits> __i;
@@ -129,7 +158,7 @@ public:
 
         // Ensure that valid count of the symbols was extracted from
         // the provided input stream.
-        if (__strval.length() != __count) {
+        if (int64_t(__strval.length()) != __count) {
             std::ostringstream __error;
 
             __error << "bencode::string::load the specified string "
@@ -142,38 +171,100 @@ public:
         _M_value = __strval;
     }
 
+    /**
+     *  @brief Number of CharT elements in the string.
+     *
+     *  Returns the number of characters in the string, not including
+     *  any null-termination.
+     */
+    size_type
+    length() const
+    { return _M_value.length(); }
+
+    /**
+     *  @brief Iterator to the first character.
+     *
+     *  Returns a read/write iterator that points to the first character in
+     *  the %string. Unshares the string.
+     */
     iterator
     begin()
     { return _M_value.begin(); }
 
+    /**
+     *  @brief Iterator to the last character.
+     *
+     *  Returns a read/write iterator that points one past the last
+     *  character in the %string.  Unshares the string.
+     */
     iterator
     end()
     { return _M_value.end(); }
 
+    /**
+     *  @brief Constant iterator to the first character.
+     *
+     *  Returns a read-only (constant) iterator that points to the first
+     *  character in the %string.
+     */
     const_iterator
     cbegin() const
     { return _M_value.cbegin(); }
 
+    /**
+     *  @brief Constant iterator to the last character.
+     *
+     *  Returns a read/write iterator that points one past the last
+     *  character in the %string.  Unshares the string.
+     */
     const_iterator
     cend() const
     { return _M_value.cend(); }
 
+    /**
+     *  @brief Convert to the underlying string type.
+     *
+     *  Returns the copy of the internal representation of the string.
+     */
     operator
     string_type() const
     { return _M_value; }
 
+    /**
+     *  @brief Test the equivalence of the strings.
+     *  @param __s  Source string.
+     *
+     *  Returns True if the specified string is equal to this one, False
+     *  otherwise.
+     */
     bool
     operator==(const basic_string& __s) const noexcept(true)
     { return _M_value == __s._M_value; }
 
+    /**
+     *  @brief Test the equivalcen of the strings.
+     *  @param __s  Source string.
+     *
+     *  Returns True if the specified string is equal to this one, False
+     *  otherwise.
+     */
     bool
     operator==(const string_type& __s)
     { return _M_value == __s; }
 
+    /**
+     *  @brief Write string to a stream.
+     *  @param __s  Output stream.
+     *  @param __v  String to write out.
+     *  @return  Reference to the output stream.
+     *
+     *  Output characters of @a __v into __s following the same rules as for
+     *  writing a %string.
+     */
     friend std::basic_ostream<CharT, Traits>&
     operator<<(std::basic_ostream<CharT, Traits>& __s,
-        const basic_string& __i)
-    { __s << __i._M_value; return __s; }
+        const basic_string& __v)
+    { __s << __v._M_value; return __s; }
 
 private:
     string_type _M_value;
--- include/bencode/utility.hpp
+++ include/bencode/utility.hpp
@@ -10,16 +10,43 @@
 
 namespace bencode
 {
+inline namespace literals
+{
 
 
 std::shared_ptr<basic_value<char>>
-inline operator"" _bencode(const char* __chars, std::size_t)
+inline operator"" _bencode(const char* __str, std::size_t)
 {
-    std::istringstream __ss(__chars);
+    std::istringstream __ss(__str);
     return make_value<char>(__ss);
 }
 
 
+std::shared_ptr<basic_value<wchar_t>>
+inline operator"" _bencode(const wchar_t* __str, std::size_t)
+{
+    std::wistringstream __ss(__str);
+    return make_value<wchar_t>(__ss);
+}
+
+
+std::shared_ptr<basic_value<char16_t>>
+inline operator"" _bencode(const char16_t* __str, std::size_t)
+{
+    std::basic_stringstream<char16_t> __ss(__str);
+    return make_value<char16_t>(__ss);
+}
+
+
+std::shared_ptr<basic_value<char32_t>>
+inline operator"" _bencode(const char32_t* __str, std::size_t)
+{
+    std::basic_stringstream<char32_t> __ss(__str);
+    return make_value<char32_t>(__ss);
+}
+
+
+} // inline namespace listerals
 } // namespace bencode
 
 
